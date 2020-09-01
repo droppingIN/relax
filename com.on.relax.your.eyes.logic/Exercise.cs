c84c8657cfced79bd4e622e24afd66405aef1d8c
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-01 19:58:42 +0200

    +application logic classes

--- com.on.relax.your.eyes.logic/Exercise.cs
+++ com.on.relax.your.eyes.logic/Exercise.cs
@@ -4,5 +4,9 @@ namespace com.on.relax.your.eyes.logic
 {
     public class Exercise
     {
+        public string pathToResource = "";
+        public string description = "";
+        public string comment = "";
+        public TimeSpan duration;
     }
 }
---
+++ com.on.relax.your.eyes.logic/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace com.on.relax.your.eyes.logic
+{
+    //(State, Action) is a lightweight tuple
+    using TransitionMatrix = Dictionary<(State, Action), State>;
+
+    class StateMachine
+    {
+        public State State { get; private set; }
+
+        private readonly TransitionMatrix transitionMatrix;
+
+        public StateMachine(in TransitionMatrix transitions)
+        {
+            State = State.Off;
+            transitionMatrix = transitions;
+        }
+
+        public State SwitchState(Action action)
+        {
+            try
+            {
+                var newState = transitionMatrix[(State, action)];
+                State = newState;
+            }
+            catch (KeyNotFoundException e)
+            {
+                var message = $"Transition not found for state: {State} , action: {action}";
+                Log.Warning(message, e);
+            }
+            return State;
+        }
+
+        public static TransitionMatrix InitialTransitions()
+        {
+            var matrix = new TransitionMatrix();
+            // Lifecycle
+            matrix[(State.Off, Action.Start)] = State.On;
+            matrix[(State.On, Action.Stop)] = State.Off;
+            // Pause
+            matrix[(State.On, Action.PauseBegin)] = State.Pause;
+            matrix[(State.Pause, Action.PauseEnd)] = State.On;
+            // Exercise
+            matrix[(State.On, Action.ExerciseSuggest)] = State.ExerciseSuggested;
+            matrix[(State.ExerciseSuggested, Action.ExerciseAccept)] = State.Exercise;
+            matrix[(State.ExerciseSuggested, Action.ExercisePostpone)] = State.On;
+            matrix[(State.Exercise, Action.ExerciseEnd)] = State.On;
+            return matrix;
+        }
+    }
+}
