da40dbe675bf721ff88af143dd62f4c6c562d4a0
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-03 22:32:55 +0200

    +interface for state machine

---
+++ com.on.relax.your.eyes.logic/IStateMachine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace com.on.relax.your.eyes.logic
+{
+    //(State, Action) is a lightweight tuple
+    public interface IStateMachine
+    {
+        State SwitchState(Action action);
+        State State { get; }
+    }
+}
