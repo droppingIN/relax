70e6dbac308d18a8cf4a699aa20f40158f4b96d6
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-06 18:12:36 +0200

    +grid on workpage with buttons and Label
    allows to switch state and displays current state

--- com.on.relax.your.eyes.xam/WorkPage.xaml.cs
+++ com.on.relax.your.eyes.xam/WorkPage.xaml.cs
@@ -1,14 +1,40 @@
-﻿using Xamarin.Forms;
+﻿using com.on.relax.your.eyes.logic;
+using System.Windows.Input;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Action = com.on.relax.your.eyes.logic.Action;
 
 namespace com.on.relax.your.eyes.xam
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WorkPage : ContentPage
     {
+        private State state;
+        public State State
+        {
+            get => state;
+            set
+            {
+                if(value != state)
+                {
+                    state = value;
+                    OnPropertyChanged(nameof(State));
+                }
+            }
+        }
+
         public WorkPage()
         {
             InitializeComponent();
+            State = StateMachineProvider.Get().State;
+
+            OnClick = new Command<Action>((Action action) =>
+            {
+                State = StateMachineProvider.Get().SwitchState(action);
+            });
+            BindingContext = this;
         }
+
+        public ICommand OnClick { private set; get; }
     }
 }
