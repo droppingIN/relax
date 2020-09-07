caa34596db6876283d925bda790dcab2e5087d70
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-07 21:56:13 +0200

    IStateMachine cleanup

--- com.on.relax.your.eyes.logic/IStateMachine.cs
+++ com.on.relax.your.eyes.logic/IStateMachine.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-
-namespace com.on.relax.your.eyes.logic
+﻿namespace com.on.relax.your.eyes.logic
 {
-    //(State, Action) is a lightweight tuple
     public interface IStateMachine
     {
         State SwitchState(Action action);
