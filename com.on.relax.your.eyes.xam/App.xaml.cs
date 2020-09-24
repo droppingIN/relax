e7415204fa6f8a9adaa34e40a97b0438c5726382
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-24 23:58:05 +0200

    App: split ctor on methods

--- com.on.relax.your.eyes.xam/App.xaml.cs
+++ com.on.relax.your.eyes.xam/App.xaml.cs
@@ -1,31 +1,27 @@
-using System;
+using com.on.relax.your.eyes.logic;
 using Xamarin.Forms;
 
 namespace com.on.relax.your.eyes.xam
 {
     public partial class App : Application
     {
+        private PropertyHandler properties;
         public App()
         {
             InitializeComponent();
 
-            var skipHelloKey = nameof(Settings.SkipHello);
-            if (Current.Properties.ContainsKey(skipHelloKey))
-                Settings.Instance.SkipHello = (bool)Current.Properties[skipHelloKey];
+            properties = new PropertyHandler(Current.Properties);
+
+            if (null == StateMachineProvider.Get())
+            {
+                InitStateMachine(Current, properties);
+            }
 
             MainPage = new NavigationPage(new WorkPage());
 
-            if (!Settings.Instance.SkipHello)
-            {
-                Action tapped = () =>
-                {
-                    MainPage.Navigation.PopAsync();
-                    Settings.Instance.SkipHello = true;
-                    Current.Properties[skipHelloKey] = Settings.Instance.SkipHello;
-                    Current.SavePropertiesAsync();
-                };
-                MainPage.Navigation.PushAsync(new HelloPage(tapped));
-            }
+            var skipHello = properties.Get(Settings.Instance.SkipHello, false);
+            if (!skipHello)
+                MainPage.Navigation.PushAsync(new HelloPage(CloseHelloPage));
         }
 
         protected override void OnStart()
@@ -38,6 +34,25 @@ namespace com.on.relax.your.eyes.xam
 
         protected override void OnResume()
         {
+        }
+
+        private void CloseHelloPage()
+        {
+            MainPage.Navigation.PopAsync();
+            properties.Set(Settings.Instance.SkipHello, true);
+            Current.SavePropertiesAsync();
+        }
+
+        private void InitStateMachine(Application current, PropertyHandler props)
+        {
+            var stateAtStartup = (State)properties.Get(Settings.Instance.StateKey, (int)State.Unknown);
+            if (State.Unknown == stateAtStartup)
+            {
+                stateAtStartup = State.Off;
+                props.Set(Settings.Instance.StateKey, (int)stateAtStartup);
+                current.SavePropertiesAsync();
+            }
+            StateMachineProvider.Initilaize(stateAtStartup);
         }
     }
 }
