88f60fe31c2db7de8e12e89042204529913cef83
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-24 23:58:05 +0200

    +ActionExtension: for SysyemAction
    +FireAndReset, +ResetAll

---
+++ com.on.relax.your.eyes.logic/ActionExtension.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System
+{
+    public static partial class ActionExtension
+    {
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void FireAndReset(this Action handler)
+        {
+            if (null != handler)
+            {
+                handler();
+            }
+            ResetAll(handler);
+        }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static void ResetAll(this Action handler)
+        {
+            if (null != handler)
+            {
+                foreach (var invocation in handler.GetInvocationList())
+                {
+                    handler -= (Action)invocation;
+                }
+            }
+        }
+    }
+}
