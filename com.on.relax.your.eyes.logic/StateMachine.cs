17af5020bf6fd026b5bad83f99430dd56b61e497
sean <37913360+droppingIN@users.noreply.github.com>
2020-09-24 23:58:05 +0200

    StateMachine: get rid of exception

--- com.on.relax.your.eyes.logic/StateMachine.cs
+++ com.on.relax.your.eyes.logic/StateMachine.cs
@@ -19,16 +19,9 @@ namespace com.on.relax.your.eyes.logic
 
         public State SwitchState(Action action)
         {
-            try
-            {
-                var newState = transitionMatrix[(State, action)];
-                State = newState;
-            }
-            catch (KeyNotFoundException e)
-            {
-                var message = $"Transition not found for state: {State} , action: {action}";
-                Log.Warning(message, e);
-            }
+            var key = (State, action);
+            if (transitionMatrix.ContainsKey(key))
+                State = transitionMatrix[key];
             return State;
         }
 
